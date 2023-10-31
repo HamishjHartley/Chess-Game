0814294425e0a173f0e5e9a305852720231dcbc6
“HamishjHartley” <hamishjhartley@gmail.com>
2023-10-31 09:36:59 +0000

    Started Pawn child class

--- main.py
+++ main.py
@@ -4,14 +4,16 @@
 
 import numpy as np
 
+#TODO: possibly create Move class, or move this to Board class
 move_stack =[]
 
-#Peice class
+#Peice Parent class, from which all peice types inherit
 class Peice:
     #Constructor which initiialzes the x and y position of the Peice
     def __init__(self):
         self.pos_x = 0
         self.pos_y = 0
+        self.legal_moves =[]
         
     #Updates the associated x and y postion so the peice can keep track of it's location
     def updatePosition(self, pos_x, pos_y):
@@ -22,25 +24,42 @@ class Peice:
     def currentPosition(self):
         return self.pos_x, self.pos_y
     
-#board class
+#Pawn class which is a child class of Peice
+class Pawn(Peice):
+    def __init__(self):
+        Peice.__init__(self) #to keep the inheritance of Peice's "__init__" function
+
+    #Returns a list of legal move(s) 
+    def getLegalMoves(self):
+        #Search through adjecent squares in board array, 
+        #by adding [x, y]
+        #if sqaure is empty:
+        #   self.legal_moves.append(square index)
+        return self.legal_moves
+
+
+#Board class
+#is the controller class to which "Peices" are added, 
 class Board:
     #constructor to initialize 8x8 board array
     def __init__(self):
         self.board = np.zeros((8,8), dtype=Peice)
 
     #Adds a peice to a position on the 8x8 board array defined by index (pos)
+    #CAREFUL: pos_x and pos_y not aligned with outputted array axes
     def addPeice(self, Peice, pos_x, pos_y):
         self.board[pos_x,pos_y]= Peice
         Peice.updatePosition(pos_x, pos_y)
 
     #Removes a given peice from the 8x8 board array(By replacing with a 0)
-    #Find a better way to do this
+    #TODO:Find a better way to do this
     def removePeice(self, Peice):
         #np.delete(self.board, [Peice.currentPosition()])
         self.board[Peice.currentPosition()]= 0
     
     # #Moves a given peice to a target position
     #by removing from origional square and adding it to target square
+    #TODO:Find better way to do this
     def movePeice(self, Peice, pos_x, pos_y):
         self.board.removePeice(Peice)
         self.board.addPeice(Peice, pos_x, pos_y)

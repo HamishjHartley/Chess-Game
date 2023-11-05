2a6109fe88d310540de3a683708b848fee5dddef
“HamishjHartley” <hamishjhartley@gmail.com>
2023-11-05 09:18:36 +0000

    Implemented a stateboard to keep track of peice locations based on colour

--- main.py
+++ main.py
@@ -12,32 +12,32 @@ class Peice:
     #Constructor which initiialzes the vertial and horizontal position of the Peice
     #TODO: throw exception if colour not 0 or 1
     def __init__(self, colour):
-        self.COLOUR = colour #Colour class constant, int value either 0 or 1
-        self.pos_vert = 0
-        self.pos_hor = 0
+        self.COLOUR = colour #Colour class constant, 1 = W, -1 = B
+        self.v = 0
+        self.h = 0
         self.legal_moves =[]
         self.is_captured = False
         
     #Updates the associated vertical and horizontal postion so the peice can keep track of it's location
-    def update_position(self, pos_vert, pos_hor):
-        self.pos_vert = pos_vert
-        self.pos_hor = pos_hor
+    def update_position(self, v, h):
+        self.v = v
+        self.h = h
 
     #Returns the current location of the peice
     def current_position(self):
-        return self.pos_vert, self.pos_hor
+        return self.v, self.h
     
 #Pawn class which is a child class of Peice
 class Pawn(Peice):
-    def __init__(self):
-        Peice.__init__(self) #to keep the inheritance of Peice's "__init__" function
+    def __init__(self, colour):
+        Peice.__init__(self, colour) #to keep the inheritance of Peice's "__init__" function
 
     #Returns a list of legal move(s) specific to a Pawn peice
     def get_legal_moves(self):
         #Pawns movement rules
-        self.straight_ahead = (self.pos_vert+1, self.pos_hor)
-        self.capture_left = (self.pos_vert+1, self.pos_hor-1)
-        self.capture_right = (self.pos_vert+1, self.pos_hor+1)
+        self.straight_ahead = (self.v+1, self.h)
+        self.capture_left = (self.v+1, self.h-1)
+        self.capture_right = (self.v+1, self.h+1)
 
         #Search through adjecent squares in board array, 
         if play_board.board[self.straight_ahead] == 0:
@@ -50,6 +50,14 @@ class Pawn(Peice):
         #elif (capture square RIGHT) has enemyPeice:
         #   self.legal_moves.append(square index)
         return self.legal_moves
+    
+    # class Bishop(Peice):
+    #     def __init__(self, colour):
+    #         Peice.__init__(self) #to keep the inheritance of Peice's "__init__" function
+
+    #     def get_legal_moves(self):
+    #         #up right grid travelersal
+    #         return self.legal_moves
 
 
 #Board class
@@ -59,11 +67,18 @@ class Board:
     def __init__(self):
         self.board = np.zeros((8,8), dtype=Peice)
 
+        #"bit board" to hold the state of each square on the board for easier move/capture calculation
+        self.bit_board = np.zeros((8,8), dtype=int)
+
+
     #Adds a peice to a position on the 8x8 board array defined by index (pos)
     #TODO: Throw exception if position reference out of bounds of board array
-    def add_peice(self, Peice, pos_vert, pos_hor):
-        self.board[pos_vert,pos_hor]= Peice
-        Peice.update_position(pos_vert, pos_hor)
+    def add_peice(self, Peice, v, h):
+        self.board[v,h]= Peice
+        Peice.update_position(v, h)
+
+        #Simulatniously update bit board with the associated integer value for each sqaure. 
+        self.bit_board[v, h] = Peice.COLOUR
 
     #Removes a given peice from the 8x8 board array(By replacing with a 0)
     #TODO:Find a better way to do this
@@ -74,18 +89,19 @@ class Board:
     # #Moves a given peice to a target position
     #by removing from origional square and adding it to target square
     #TODO:Find better way to do this
-    def move_peice(self, Peice, pos_vert, pos_hor):
+    def move_peice(self, Peice, v, h):
         self.board.remove_peice(Peice)
-        self.board.add_peice(Peice, pos_vert, pos_hor)
+        self.board.add_peice(Peice, v, h)
 
 play_board = Board() 
 
-pawn1 = Pawn(0) #White pawn    
-pawn2 = Pawn(1) #Black pawn
+pawn1 = Pawn(1) #White pawn    
+pawn2 = Pawn(-1) #Black pawn
 
 play_board.add_peice(pawn1,1,4) #Add e2 pawn to board (WHITE)
 play_board.add_peice(pawn2,6,3) #Add d7 pawn to board (BLACK)
 
+print(play_board.bit_board)
 
-print(pawn1.get_legal_moves())
-print(pawn2.get_legal_moves())
+# print(pawn1.get_legal_moves())
+# print(pawn2.get_legal_moves())
